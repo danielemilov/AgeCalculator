eda44830761ef30e002468946e0ef02e43e955a7
Daniel Emilov <152512642+danielemilov@users.noreply.github.com>
2024-06-16 23:10:12 +0200

    fixed the path of the audio

--- src/App.jsx
+++ src/App.jsx
@@ -11,39 +11,31 @@ const AppWrapper = styled.div`
   background-color: #0d009d53;
   color: white;
   text-align: center;
-  border-radius:45px;
+  border-radius: 45px;
   box-shadow: 0 0 10px rgb(0, 0, 0);
   display: flex;
   flex-direction: column;
   justify-content: center;
   @media (max-width: 768px) {
-    padding-bottom: 10rem;
-    padding-top: 10rem;
     padding: 3rem;
     margin: 5rem auto;
-
   }
 `;
 
 const Title = styled.h1`
   color: white;
-  text-align: center;
-
-
 `;
 
 const FormWrapper = styled.form`
   display: flex;
   flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Label = styled.label`
   display: block;
-  margin-bottom: 10px;
   margin: 20px;
-  width: max-content;
 `;
 
 const Input = styled.input`
@@ -127,14 +119,14 @@ const App = () => {
       return;
     }
 
-    let age = today.getFullYear() - birthDate.getFullYear();
+    let calculatedAge = today.getFullYear() - birthDate.getFullYear();
     const monthDifference = today.getMonth() - birthDate.getMonth();
 
     if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
-      age--;
+      calculatedAge--;
     }
 
-    setAge(age);
+    setAge(calculatedAge);
     setError('');
 
     if (birthDate.getMonth() === today.getMonth() && birthDate.getDate() === today.getDate()) {
@@ -168,40 +160,40 @@ const App = () => {
 
   return (
     <>
-    <div className="wrapper">
-    <h2>"Calculate Your Age, Not Your Calories - It's Free!" LMAO</h2>
+      <div className="wrapper">
+        <h2>"Calculate Your Age, Not Your Calories - It's Free!"</h2>
       </div>
-    <AppWrapper>
-      <Title>AGE CALCULATOR</Title>
-      <FormWrapper onSubmit={handleSubmit}>
-        <Label>
-          Enter your birthdate:
-          <Input
-            type="date"
-            value={birthdate}
-            onChange={handleInputChange}
-            placeholder="YYYY-MM-DD"
-          />
-        </Label>
-        <Button type="submit">Calculate Age</Button>
-      </FormWrapper>
+      <AppWrapper>
+        <Title>AGE CALCULATOR</Title>
+        <FormWrapper onSubmit={handleSubmit}>
+          <Label>
+            Enter your birthdate:
+            <Input
+              type="date"
+              value={birthdate}
+              onChange={handleInputChange}
+              placeholder="YYYY-MM-DD"
+            />
+          </Label>
+          <Button type="submit">Calculate Age</Button>
+        </FormWrapper>
 
-      {age !== null && !isNaN(age) && (
-        <>
-          <AgeMessage>Your age is: {age}</AgeMessage>
-          {isBirthday && (
-            <>
-              <AgeMessage>Happy Birthday!</AgeMessage>
-              <Confetti />
-              <AudioElement ref={audioRef} autoPlay>
-                <source src="src/assets/Happy Birthday - Stevie Wonder.mp3" type="audio/mpeg" />
-              </AudioElement>
-            </>
-          )}
-        </>
-      )}
-      {error && <ErrorMessage>{error}</ErrorMessage>}
-    </AppWrapper>
+        {age !== null && !isNaN(age) && (
+          <>
+            <AgeMessage>Your age is: {age}</AgeMessage>
+            {isBirthday && (
+              <>
+                <AgeMessage>Happy Birthday!</AgeMessage>
+                <Confetti />
+                <AudioElement ref={audioRef} autoPlay>
+                  <source src="./src/assets/Happy Birthday - Stevie Wonder.mp3" type="audio/mpeg" />
+                </AudioElement>
+              </>
+            )}
+          </>
+        )}
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+      </AppWrapper>
     </>
   );
 };
